0c5d41f6ae3174483948fb5f51822f88d527c6bf
Ricardo R <ricardorojas320@aragon.unam.mx>
2025-02-12 20:22:07 -0600

    Proyecto1

---
+++ TareasCasa/webLibreria/autorizacion.php
@@ -0,0 +1,16 @@
+<?php require "./header.php"; ?>
+
+
+<div class="row" style="margin-top:50px">
+    <div class="col s6 offset-s3">
+        <div>
+            <div>
+                <div class="center-align">
+                    <h4 class="center">No se puede acceder a este apartado</h4>
+                    <h5 class="center">"Sesion no iniciada"</h5>
+                    <a class='btn-large waves-effect waves-light blue lighten-1' href='login.php' >Intenta ingresar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ TareasCasa/webLibreria/logica/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+    $host_db="127.0.0.1:3306";
+    $user_name="root";
+    $user_pass="berserk";
+    $db_name="libreria";
+
+    $conexion = new mysqli($host_db,$user_name,$user_pass,$db_name);
+    
+    if($conexion->connect_error){
+      }
+      else{
+        echo "<h1></h1>";
+      } 
+?>
